3feae4ef19f3a3bc937e7abf6950bda1d1d87659
Dorien Michael Gardner <dgardner@rocketsoftware.com>
2015-07-06 21:46:37 -0600

    Revert "[D3 Samples]"
    This reverts commit ca988623588a48fd11f375ffae35a88b1daa2b82.

--- D3 MVSP/MVSPNetAPI/MVSPNETAPI/samples/Customer Maintenance/CustMaintSource2008/CustomerMaint/CustMaintCommon.vb
+++
@@ -1,8 +0,0 @@
-﻿
-Module CustMaintCommon
-    Public MVSP As New rocketsoftware.MVSP.Pick
-    Public mvspfunctions As New rocketsoftware.MVSP.Functions
-    Public currentitemid As String = ""
-    Public currentitem As String = ""
-    Public AM As String = Chr(254)
-End Module
